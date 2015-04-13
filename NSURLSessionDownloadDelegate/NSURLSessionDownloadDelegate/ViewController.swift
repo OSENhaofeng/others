0a08e940e8e0155c2109b33d89a945a0ecd1743e
Carlos Butron <carlosbutron@icloud.com>
2015-04-13 12:52:45 +0200

    Updated to Swift 1.2. Translated to English. Added new license GPL v3.0.

--- NSURLSessionDownloadDelegate/NSURLSessionDownloadDelegate/ViewController.swift
+++ NSURLSessionDownloadDelegate/NSURLSessionDownloadDelegate/ViewController.swift
@@ -3,7 +3,15 @@
 //  NSURLSessionDownloadDelegate
 //
 //  Created by Carlos Butron on 02/12/14.
-//  Copyright (c) 2014 Carlos Butron. All rights reserved.
+//  Copyright (c) 2014 Carlos Butron.
+//
+//  This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
+//  License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later
+//  version.
+//  This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+//  warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+//  You should have received a copy of the GNU General Public License along with this program. If not, see
+//  http:/www.gnu.org/licenses/.
 //
 
 import UIKit
@@ -39,7 +47,7 @@ class ViewController: UIViewController, NSURLSessionDownloadDelegate {
     }
     
     func URLSession(session: NSURLSession, downloadTask: NSURLSessionDownloadTask, didFinishDownloadingToURL location: NSURL){
-        println("Descarga Terminada")
+        println("Download finished")
         var downloadedImage = UIImage(data: NSData(contentsOfURL: location)!)
         dispatch_async(dispatch_get_main_queue(), {() in
             self.imagen.image = downloadedImage
