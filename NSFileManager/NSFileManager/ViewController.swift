2d0ea28555b356aae5a016c34c851fd61468ca79
Carlos Butron <carlosbutron@icloud.com>
2015-04-13 02:19:56 +0200

    Updated to Swift 1.2. Translated to English. Added new license GPL v3.0.

--- NSFileManager/NSFileManager/ViewController.swift
+++ NSFileManager/NSFileManager/ViewController.swift
@@ -3,7 +3,15 @@
 //  NSFileManager
 //
 //  Created by Carlos Butron on 06/12/14.
-//  Copyright (c) 2014 Carlos Butron. All rights reserved.
+//  Copyright (c) 2014 Carlos Butron.
+//
+//  This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
+//  License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later
+//  version.
+//  This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+//  warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+//  You should have received a copy of the GNU General Public License along with this program. If not, see
+//  http:/www.gnu.org/licenses/.
 //
 
 import UIKit
@@ -65,9 +73,9 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         var isDir: ObjCBool = ObjCBool(false)
         if(fileManager.fileExistsAtPath(documentsPath.stringByAppendingPathComponent(files[indexPath.row] as String), isDirectory: &isDir)){
             if(isDir){
-                cell.imageView.image = UIImage(named: "directorio.png")
+                cell.imageView.image = UIImage(named: "dir.png")
             } else{
-                cell.imageView.image = UIImage(named: "fichero.png")
+                cell.imageView.image = UIImage(named: "file.png")
             }
         }
         
