c5e3146162c2a99751ddd81654afde5bf4d9b168
Carlos Butron <carlosbutron@icloud.com>
2015-11-07 17:20:23 +0100

    Updated to Swift 2.0

--- SocialFrameworkTwitter/SocialFrameworkTwitter/FirstViewController.swift
+++ SocialFrameworkTwitter/SocialFrameworkTwitter/FirstViewController.swift
@@ -33,25 +33,24 @@ class FirstViewController: UIViewController, UITextFieldDelegate {
             checkUser() //method implemented after
         }
             else{
-            println("Introduce a name!")
+            print("Introduce a name!")
         }
         
     }
     
     @IBAction func sendTweet(sender: UIButton) {
-        var twitterController = SLComposeViewController(forServiceType:SLServiceTypeTwitter)
-        var imageToTweet = UIImage(named:"image1.png")
+        let twitterController = SLComposeViewController(forServiceType:SLServiceTypeTwitter)
+        let imageToTweet = UIImage(named:"image1.png")
         twitterController.setInitialText("Test from Xcode")
         twitterController.addImage(imageToTweet)
-        var completionHandler:SLComposeViewControllerCompletionHandler = {(result) in
+        let completionHandler:SLComposeViewControllerCompletionHandler = {(result) in
         twitterController.dismissViewControllerAnimated(true, completion: nil)
             switch(result) {
             case SLComposeViewControllerResult.Cancelled:
-            println("Canceled")
+            print("Canceled")
             case SLComposeViewControllerResult.Done:
-            println("User tweeted")
-            default:
-            println("Canceled")
+            print("User tweeted")
+            
             } }
             twitterController.completionHandler = completionHandler
             self.presentViewController(twitterController, animated: true, completion: nil)
@@ -78,49 +77,49 @@ class FirstViewController: UIViewController, UITextFieldDelegate {
     
     
     func checkUser(){
-        var accountStore = ACAccountStore()
-        var accountType = accountStore.accountTypeWithAccountTypeIdentifier(ACAccountTypeIdentifierTwitter)
+        let accountStore = ACAccountStore()
+        let accountType = accountStore.accountTypeWithAccountTypeIdentifier(ACAccountTypeIdentifierTwitter)
         accountStore.requestAccessToAccountsWithType(accountType, options: nil, completion: {(granted, error) in
                 if(granted == true) {
                 var accounts = accountStore.accountsWithAccountType(accountType)
                 if(accounts.count>0){
-                var twitterAccount = accounts[0] as! ACAccount
-                var twitterInfoRequest = SLRequest(forServiceType: SLServiceTypeTwitter, requestMethod: SLRequestMethod.GET, URL: NSURL(string: "https://api.twitter.com/1.1/users/show.json"), parameters: NSDictionary(object: self.labelUserName.text, forKey: "screen_name") as [NSObject : AnyObject])
+                let twitterAccount = accounts[0] as! ACAccount
+                let twitterInfoRequest = SLRequest(forServiceType: SLServiceTypeTwitter, requestMethod: SLRequestMethod.GET, URL: NSURL(string: "https://api.twitter.com/1.1/users/show.json"), parameters: NSDictionary(object: self.labelUserName.text!, forKey: "screen_name") as [NSObject : AnyObject])
                 twitterInfoRequest.account = twitterAccount
                 twitterInfoRequest.performRequestWithHandler({(responseData,urlResponse,error) in
                 dispatch_async(dispatch_get_main_queue(), {()
                 in
                 //check access
                 if(urlResponse.statusCode == 429){
-                println("Rate limit reached")
+                print("Rate limit reached")
                 return
                 }
                 //check error
                 if((error) != nil){
-                println("Error \(error.localizedDescription)")
+                print("Error \(error.localizedDescription)")
                 }
                 if(responseData != nil){
                 //if dta received TO DO
-                var error: NSError?
-                var TWData = NSJSONSerialization.JSONObjectWithData(responseData, options: NSJSONReadingOptions.MutableLeaves, error: &error) as! NSDictionary
+                
+                let TWData = (try! NSJSONSerialization.JSONObjectWithData(responseData, options: NSJSONReadingOptions.MutableLeaves)) as! NSDictionary
                 //getting data
-                var followers = TWData.objectForKey("followers_count")!.integerValue
-                var following = TWData.objectForKey("friends_count")!.integerValue!
-                var description = TWData.objectForKey("description") as! NSString
+                let followers = TWData.objectForKey("followers_count")!.integerValue
+                let following = TWData.objectForKey("friends_count")!.integerValue!
+                let description = TWData.objectForKey("description") as! NSString
                 self.labelFollowers.text = "\(followers)"
                 self.labelFollowing.text = "\(following)"
                 self.textViewDescription.text = description as String
                 
                 var profileImageStringURL = TWData.objectForKey("profile_image_url_https") as! NSString
                 profileImageStringURL = profileImageStringURL.stringByReplacingOccurrencesOfString("_normal", withString: "")
-                var url = NSURL(string: profileImageStringURL as String)
-                var data = NSData(contentsOfURL: url!)
+                let url = NSURL(string: profileImageStringURL as String)
+                let data = NSData(contentsOfURL: url!)
                 self.imageViewPhoto.image = UIImage(data: data!)
                 
                 } })
                 }) }
     } else {
-    println("No access granted")
+    print("No access granted")
     }
     }) }
 
--- SocialFrameworkTwitter/SocialFrameworkTwitter/SecondViewController.swift
+++ SocialFrameworkTwitter/SocialFrameworkTwitter/SecondViewController.swift
@@ -43,30 +43,47 @@ class SecondViewController: UIViewController, UITableViewDataSource, UITableView
     
     func refreshTimeLine(){
             if(SLComposeViewController.isAvailableForServiceType(SLServiceTypeTwitter)){
-            var accountStore = ACAccountStore()
-            var accountType = accountStore.accountTypeWithAccountTypeIdentifier(ACAccountTypeIdentifierTwitter)
+            let accountStore = ACAccountStore()
+            let accountType = accountStore.accountTypeWithAccountTypeIdentifier(ACAccountTypeIdentifierTwitter)
             accountStore.requestAccessToAccountsWithType(accountType, options: nil, completion: {(granted,error) in
             if(granted==true){
-            var arrayOfAccounts = accountStore.accountsWithAccountType(accountType)
-            var tempAccount = arrayOfAccounts.last as! ACAccount
-            var tweetURL = NSURL(string: "https://api.twitter.com/1.1/statuses/home_timeline.json")
-            var tweetRequest = SLRequest(forServiceType:SLServiceTypeTwitter, requestMethod: SLRequestMethod.GET, URL: tweetURL, parameters: nil)
+            let arrayOfAccounts = accountStore.accountsWithAccountType(accountType)
+            let tempAccount = arrayOfAccounts.last as! ACAccount
+            let tweetURL = NSURL(string: "https://api.twitter.com/1.1/statuses/home_timeline.json")
+            let tweetRequest = SLRequest(forServiceType:SLServiceTypeTwitter, requestMethod: SLRequestMethod.GET, URL: tweetURL, parameters: nil)
             tweetRequest.account = tempAccount
             tweetRequest.performRequestWithHandler({(responseData,urlResponse,error) in
             if(error == nil){
-                /*get tweets*/
-                var jsonError:NSError?
-                var responseJSON = NSJSONSerialization.JSONObjectWithData(responseData, options: NSJSONReadingOptions.AllowFragments, error: &jsonError) as! NSArray
-                if(jsonError != nil) {
-                    println("JSON ERROR ")
-                }
-                    else{
-                    self.tweetsArray = responseJSON
+                
+                let responseJSON: NSArray?
+                
+                do {
+                    
+                    responseJSON = try NSJSONSerialization.JSONObjectWithData(responseData, options: NSJSONReadingOptions.AllowFragments) as? NSArray
+                    
+                    self.tweetsArray = responseJSON!
                     self.imageDictionary = NSMutableDictionary()
                     dispatch_async(dispatch_get_main_queue(),
                     {self.table.reloadData()})
-                }}
-            else{ /*access Error*/ }
+                    
+                } catch _ {
+                    
+                    print("JSON ERROR ")
+                    
+                }
+//                /*get tweets*/
+//                let jsonError:NSError?
+//                let responseJSON = NSJSONSerialization.JSONObjectWithData(responseData, options: NSJSONReadingOptions.AllowFragments, error: &jsonError) as! NSArray
+//                if(jsonError != nil) {
+//                    print("JSON ERROR ")
+//                }
+//                    else{
+//                    self.tweetsArray = responseJSON
+//                    self.imageDictionary = NSMutableDictionary()
+//                    dispatch_async(dispatch_get_main_queue(),
+//                    {self.table.reloadData()})
+//                }}
+            } else{ /*access Error*/ }
             })
             } })
         }
@@ -81,18 +98,18 @@ class SecondViewController: UIViewController, UITableViewDataSource, UITableView
 
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        var cell = tableView.dequeueReusableCellWithIdentifier("timelineCell") as! TimelineCell
-        var currentTweet = self.tweetsArray.objectAtIndex(indexPath.row) as! NSDictionary
-        var currentUser = currentTweet["user"] as! NSDictionary
+        let cell = tableView.dequeueReusableCellWithIdentifier("timelineCell") as! TimelineCell
+        let currentTweet = self.tweetsArray.objectAtIndex(indexPath.row) as! NSDictionary
+        let currentUser = currentTweet["user"] as! NSDictionary
         cell.userNameLabel!.text = currentUser["name"] as? String
         cell.tweetlabel!.text = currentTweet["text"] as! String
-        var userName = cell.userNameLabel.text
+        let userName = cell.userNameLabel.text
         if((self.imageDictionary[userName!]) != nil){
         cell.userImageView.image = (self.imageDictionary[userName!] as! UIImage)
     } else{
         dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), {
-        var imageURL = NSURL(string: currentUser.objectForKey("profile_image_url") as! String)
-        var imageData = NSData(contentsOfURL: imageURL!)
+        let imageURL = NSURL(string: currentUser.objectForKey("profile_image_url") as! String)
+        let imageData = NSData(contentsOfURL: imageURL!)
         self.imageDictionary.setObject(UIImage(data: imageData!)!, forKey: userName!)
         (dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), {
         cell.userImageView.image = (self.imageDictionary[cell.userNameLabel.text!] as! UIImage)
