85c1d8e55feedb2013b3d721283a531e721e53d7
Carlos Butron <carlosbutron@icloud.com>
2015-11-22 15:33:45 +0100

    Updated to Swift 2.0

---
+++ SwiftLoginScreen/SwiftLoginScreen/HomeVC.swift
@@ -0,0 +1,64 @@
+//
+//  HomeVC.swift
+//  SwiftLoginScreen
+//
+//  Created by Carlos Butron on 12/04/14.
+//  Copyright (c) 2015 Carlos Butron. All rights reserved.
+//
+//  This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
+//  License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later
+//  version.
+//  This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+//  warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+//  You should have received a copy of the GNU General Public License along with this program. If not, see
+//  http:/www.gnu.org/licenses/.
+//
+
+import UIKit
+
+class HomeVC: UIViewController {
+
+    @IBOutlet var usernameLabel : UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(true)
+        
+        let prefs:NSUserDefaults = NSUserDefaults.standardUserDefaults()
+        let isLoggedIn:Int = prefs.integerForKey("ISLOGGEDIN") as Int
+        if (isLoggedIn != 1) {
+            self.performSegueWithIdentifier("goto_login", sender: self)
+        } else {
+            self.usernameLabel.text = prefs.valueForKey("USERNAME") as? String
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // #pragma mark - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue?, sender: AnyObject?) {
+        // Get the new view controller using [segue destinationViewController].
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    @IBAction func logoutTapped(sender : UIButton) {
+        
+        let appDomain = NSBundle.mainBundle().bundleIdentifier
+        NSUserDefaults.standardUserDefaults().removePersistentDomainForName(appDomain!)
+        
+        self.performSegueWithIdentifier("goto_login", sender: self)
+    }
+}
---
+++ SwiftLoginScreen/SwiftLoginScreen/SignupVC.swift
@@ -0,0 +1,173 @@
+//
+//  SignupVC.swift
+//  SwiftLoginScreen
+//
+//  Created by Carlos Butron on 12/04/14.
+//  Copyright (c) 2015 Carlos Butron. All rights reserved.
+//
+//  This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
+//  License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later
+//  version.
+//  This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+//  warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+//  You should have received a copy of the GNU General Public License along with this program. If not, see
+//  http:/www.gnu.org/licenses/.
+//
+
+import UIKit
+
+class SignupVC: UIViewController {
+    
+    @IBOutlet var txtUsername : UITextField!
+    @IBOutlet var txtPassword : UITextField!
+    @IBOutlet var txtConfirmPassword : UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    /*
+    // #pragma mark - Navigation
+    
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue?, sender: AnyObject?) {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+    }
+    */
+    
+    @IBAction func gotoLogin(sender : UIButton) {
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    
+    @IBAction func signupTapped(sender : UIButton) {
+        let username:NSString = txtUsername.text! as NSString
+        let password:NSString = txtPassword.text! as NSString
+        let confirm_password:NSString = txtConfirmPassword.text! as NSString
+        
+        if ( username.isEqualToString("") || password.isEqualToString("") ) {
+            
+            let alertView:UIAlertView = UIAlertView()
+            alertView.title = "Sign Up Failed!"
+            alertView.message = "Please enter Username and Password"
+            alertView.delegate = self
+            alertView.addButtonWithTitle("OK")
+            alertView.show()
+        } else if ( !password.isEqual(confirm_password) ) {
+            
+            let alertView:UIAlertView = UIAlertView()
+            alertView.title = "Sign Up Failed!"
+            alertView.message = "Passwords doesn't Match"
+            alertView.delegate = self
+            alertView.addButtonWithTitle("OK")
+            alertView.show()
+        } else {
+            
+            let post:NSString = "username=\(username)&password=\(password)&c_password=\(confirm_password)"
+            
+            NSLog("PostData: %@",post);
+            
+            let url:NSURL = NSURL(string: "http://carlosbutron.es/iOS/jsonsignup.php")!
+            
+            let postData:NSData = post.dataUsingEncoding(NSASCIIStringEncoding)!
+            
+            let postLength:NSString = String( postData.length )
+            
+            let request:NSMutableURLRequest = NSMutableURLRequest(URL: url)
+            request.HTTPMethod = "POST"
+            request.HTTPBody = postData
+            request.setValue(postLength as String, forHTTPHeaderField: "Content-Length")
+            request.setValue("application/x-www-form-urlencoded", forHTTPHeaderField: "Content-Type")
+            request.setValue("application/json", forHTTPHeaderField: "Accept")
+            
+            
+            var reponseError: NSError?
+            var response: NSURLResponse?
+            
+            var urlData: NSData?
+            do {
+                urlData = try NSURLConnection.sendSynchronousRequest(request, returningResponse:&response)
+            } catch let error as NSError {
+                reponseError = error
+                urlData = nil
+            }
+            
+            if ( urlData != nil ) {
+                let res = response as! NSHTTPURLResponse!;
+                
+                NSLog("Response code: %ld", res.statusCode);
+                
+                if (res.statusCode >= 200 && res.statusCode < 300)
+                {
+                    let responseData:NSString  = NSString(data:urlData!, encoding:NSUTF8StringEncoding)!
+                    
+                    NSLog("Response ==> %@", responseData);
+                    
+                    var error: NSError?
+                    
+                    let jsonData:NSDictionary = (try! NSJSONSerialization.JSONObjectWithData(urlData!, options:NSJSONReadingOptions.MutableContainers )) as! NSDictionary
+                    
+                    
+                    let success:NSInteger = jsonData.valueForKey("success") as! NSInteger
+                    
+                    //[jsonData[@"success"] integerValue];
+                    
+                    NSLog("Success: %ld", success);
+                    
+                    if(success == 1)
+                    {
+                        NSLog("Sign Up SUCCESS");
+                        self.dismissViewControllerAnimated(true, completion: nil)
+                    } else {
+                        var error_msg:NSString
+                        
+                        if jsonData["error_message"] as? NSString != nil {
+                            error_msg = jsonData["error_message"] as! NSString
+                        } else {
+                            error_msg = "Unknown Error"
+                        }
+                        let alertView:UIAlertView = UIAlertView()
+                        alertView.title = "Sign Up Failed!"
+                        alertView.message = error_msg as String
+                        alertView.delegate = self
+                        alertView.addButtonWithTitle("OK")
+                        alertView.show()
+                        
+                    }
+                    
+                } else {
+                    let alertView:UIAlertView = UIAlertView()
+                    alertView.title = "Sign Up Failed!"
+                    alertView.message = "Connection Failed"
+                    alertView.delegate = self
+                    alertView.addButtonWithTitle("OK")
+                    alertView.show()
+                }
+            }  else {
+                let alertView:UIAlertView = UIAlertView()
+                alertView.title = "Sign in Failed!"
+                alertView.message = "Connection Failure"
+                if let error = reponseError {
+                    alertView.message = (error.localizedDescription)
+                }
+                alertView.delegate = self
+                alertView.addButtonWithTitle("OK")
+                alertView.show()
+            }
+        }
+        
+    }
+    
+    func textFieldShouldReturn(textField: UITextField!) -> Bool {   //delegate method
+        textField.resignFirstResponder()
+        return true
+    }
+}
