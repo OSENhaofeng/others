b3b2b0219b80c1042d62be81d59164278ffc35c3
Innoventes Technologies <mukesh9039@gmail.com>
2015-12-09 18:06:17 +0530

    Added CheckConnectivity project

---
+++ CheckConnectivity/CheckConnectivity/Connectivity.swift
@@ -0,0 +1,35 @@
+//
+//  Connectivity.swift
+//  CheckConnectivity
+//
+//  Created by Mukesh Thawani on 09/12/15.
+//  Copyright © 2015 Mukesh Thawani. All rights reserved.
+//
+
+import Foundation
+import SystemConfiguration
+
+public class Reachability {
+    
+    class func isConnectedToNetwork() -> Bool {
+        
+        var zeroAddress = sockaddr_in(sin_len: 0, sin_family: 0, sin_port: 0, sin_addr: in_addr(s_addr: 0), sin_zero: (0, 0, 0, 0, 0, 0, 0, 0))
+        zeroAddress.sin_len = UInt8(sizeofValue(zeroAddress))
+        zeroAddress.sin_family = sa_family_t(AF_INET)
+        
+        let defaultRouteReachability = withUnsafePointer(&zeroAddress) {
+            SCNetworkReachabilityCreateWithAddress(nil, UnsafePointer($0))
+        }
+        
+        var flags: SCNetworkReachabilityFlags = []
+        if SCNetworkReachabilityGetFlags(defaultRouteReachability!, &flags) == false {
+            return false
+        }
+        
+        let isReachable = flags.contains(.Reachable)
+        let needsConnection = flags.contains(.ConnectionRequired)
+        
+        return isReachable && !needsConnection
+    }
+    
+}
