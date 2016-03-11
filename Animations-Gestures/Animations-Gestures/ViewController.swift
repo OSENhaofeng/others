cde0376eb393f5934d25492a0a0f1e473ca3480a
Carlos Butron <beiosdev@icloud.com>
2016-03-11 10:30:49 +0100

    Updated to Swift 2.1

--- Animations-Gestures/Animations-Gestures/ViewController.swift
+++ Animations-Gestures/Animations-Gestures/ViewController.swift
@@ -9,7 +9,6 @@
 import UIKit
 
 class ViewController: UIViewController {
-
     @IBOutlet weak var penguinView: UIImageView!
     
     var frames: NSArray?
@@ -17,10 +16,7 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        
-        
-        
+
         let image1 = UIImage(named:"penguin_walk01")!
         let image2 = UIImage(named:"penguin_walk02")!
         let image3 = UIImage(named:"penguin_walk03")!
@@ -28,12 +24,9 @@ class ViewController: UIViewController {
         
         let frames: [UIImage] = [image1, image2, image3, image4]
         
-        
         penguinView.animationDuration = 0.15;
         penguinView.animationRepeatCount = 2;
         penguinView.animationImages = frames;
-        
-        
         
         //walk right
         let swipeGestureRight = UISwipeGestureRecognizer(target: self, action: "walkRight:")
@@ -45,32 +38,20 @@ class ViewController: UIViewController {
         swipeGestureLeft.direction = UISwipeGestureRecognizerDirection.Left
         view.addGestureRecognizer(swipeGestureLeft)
         
-
         //jump
         let tap = UITapGestureRecognizer(target: self, action: "jump:")
         view.addGestureRecognizer(tap)
         
-
         //longPress
         let longPress = UILongPressGestureRecognizer(target: self, action: "longPress:")
         view.addGestureRecognizer(longPress)
-        
-        
-        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
-
-
-
-
-
 func walkLeft(send: UIGestureRecognizer) {
-
     print("walk left");
     //CHECK IF OUT OF SCREEN
     if (penguinView.center.x < 0.0) {
@@ -84,97 +65,53 @@ func walkLeft(send: UIGestureRecognizer) {
     penguinView.startAnimating()
     
     //MOVE THE IMAGE VIEW TO LEFT
-    
-    
-    
     UIView.animateWithDuration(0.6, animations: { () -> Void in
-        
         self.penguinView.center = CGPointMake(self.penguinView.center.x - 30, self.penguinView.center.y);
-        
-        })
-    
-    
-    
+    })
 }
 
-
 func walkRight(send: UIGestureRecognizer) {
-    
     print("walk right");
-            
     if (self.penguinView.center.x > self.view.frame.size.width) {
-            self.penguinView.center = CGPointMake(0, self.penguinView.center.y);
-            }
+        self.penguinView.center = CGPointMake(0, self.penguinView.center.y);
+    }
             
     self.penguinView.transform = CGAffineTransformIdentity;
-            
     penguinView.startAnimating()
-            
     UIView.animateWithDuration(0.6, animations: { () -> Void in
-                
-    self.penguinView.center = CGPointMake(self.penguinView.center.x + 30, self.penguinView.center.y)
-                
+        self.penguinView.center = CGPointMake(self.penguinView.center.x + 30, self.penguinView.center.y)
     })
-            
 }
 
 func jump(send: UIGestureRecognizer) {
-    
     penguinView.startAnimating()
     
     UIView.animateWithDuration(0.25, animations: { () -> Void in
-        
         self.penguinView.center = CGPointMake(self.penguinView.center.x, self.penguinView.center.y - 50)
-        
-        }, completion: { (finished: Bool) -> Void in
-            
-            self.jumpBack()
+    }, completion: { (finished: Bool) -> Void in
+        self.jumpBack()
     })
-    
 }
     
-func jumpBack()
-    {
-        
-        UIView.animateWithDuration(0.25, animations: { () -> Void in
-            
-            self.penguinView.center = CGPointMake(self.penguinView.center.x, self.penguinView.center.y + 50)
-            
-        })
-       
+func jumpBack() {
+    UIView.animateWithDuration(0.25, animations: { () -> Void in
+    self.penguinView.center = CGPointMake(self.penguinView.center.x, self.penguinView.center.y + 50)
+    })
 }
 
 func longPress(send: UIGestureRecognizer) {
-    
     UIView.animateWithDuration(0.33, animations: { () -> Void in
-        
         self.dieCenter = self.penguinView.center
         self.penguinView.center = CGPointMake(self.penguinView.center.x, self.view.frame.size.height)
-        
-        }, completion: { (finished: Bool) -> Void in
-            
-            self.longPressBack()
+    }, completion: { (finished: Bool) -> Void in
+         self.longPressBack()
     })
-    
-    
 }
 
 func longPressBack() {
-    
     UIView.animateWithDuration(0.25, animations: { () -> Void in
-        
         self.penguinView.center = self.dieCenter!
-        
     })
-    
 }
 
-
-
-
 }
-
-
-
-
-
