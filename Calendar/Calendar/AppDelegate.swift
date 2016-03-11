ef23df2017e7c83611035e5e0c06bf9e2612ee98
Carlos Butron <beiosdev@icloud.com>
2016-03-11 10:36:30 +0100

    Updated to Swift 2.1

--- Calendar/Calendar/AppDelegate.swift
+++ Calendar/Calendar/AppDelegate.swift
@@ -6,14 +6,12 @@
 //  Copyright (c) 2015 Carlos Butron. All rights reserved.
 //
 
-
 import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
@@ -42,6 +40,4 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
 
-
 }
-
