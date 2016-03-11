a1b375f850c594692206ea900f93fca86dc83911
Carlos Butron <beiosdev@icloud.com>
2016-03-11 10:24:51 +0100

    Updated to Swift 2.1

--- AnimationWithCollisionQuartzCore/AnimationWithCollisionQuartzCore/AppDelegate.swift
+++ AnimationWithCollisionQuartzCore/AnimationWithCollisionQuartzCore/AppDelegate.swift
@@ -5,14 +5,12 @@
 //  Created by Carlos Butron on 02/12/14.
 //  Copyright (c) 2015 Carlos Butron. All rights reserved.
 
-
 import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
@@ -41,6 +39,4 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
 
-
 }
-
