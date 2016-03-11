a3a8e9e4197c2816a85662ab7c0fd82622a5b875
Carlos Butron <beiosdev@icloud.com>
2016-03-11 10:20:40 +0100

    Updated to Swift 2.1

--- Airdrop/Airdrop/ViewController.swift
+++ Airdrop/Airdrop/ViewController.swift
@@ -6,21 +6,16 @@
 //  Copyright (c) 2014 Carlos Butron.
 //
 
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
-
 }
-
