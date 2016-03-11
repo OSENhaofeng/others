d73528c40553690e0f4a4518c174819e0607790b
Carlos Butron <beiosdev@icloud.com>
2016-03-11 10:20:07 +0100

    Updated to Swift 2.1

--- Airdrop/Airdrop/URLViewController.swift
+++ Airdrop/Airdrop/URLViewController.swift
@@ -6,16 +6,13 @@
 //  Copyright (c) 2014 Carlos Butron.
 //
 
-
 import UIKit
 
 class URLViewController: UIViewController, UIWebViewDelegate {
     
-    
     @IBOutlet weak var myURL: UITextField!
     @IBOutlet weak var webView: UIWebView!
     
-
     @IBAction func send(sender: UIButton) {
         let url:NSURL = NSURL(string: myURL.text!)!
         let controller = UIActivityViewController(activityItems: [url], applicationActivities: nil)
@@ -27,30 +24,16 @@ class URLViewController: UIViewController, UIWebViewDelegate {
         self.webView.loadRequest(request)
     }
     
-    
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         myURL.text = "http://carlosbutron.es/"
         webView.delegate = self
         let request = NSURLRequest(URL: NSURL(string: myURL.text!)!)
-        self.webView.loadRequest(request)        // Do any additional setup after loading the view.
+        self.webView.loadRequest(request)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
