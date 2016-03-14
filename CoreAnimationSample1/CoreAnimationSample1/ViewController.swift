0019d21f71ef09a83aac0de7c6ea271e280c50bc
Carlos Butron <beiosdev@icloud.com>
2016-03-14 10:03:34 +0100

    Updated to Swift 2.1

--- CoreAnimationSample1/CoreAnimationSample1/ViewController.swift
+++ CoreAnimationSample1/CoreAnimationSample1/ViewController.swift
@@ -12,27 +12,20 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var image: UIImageView!
     @IBAction func animate(sender: UIButton) {
-        
         let animation:CABasicAnimation = CABasicAnimation(keyPath: "position")
         animation.fromValue = NSValue(CGPoint:CGPointMake(image.frame.midX, image.frame.midY))
         animation.toValue = NSValue(CGPoint:CGPointMake(image.frame.midX, 340))
         animation.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseInEaseOut)
         animation.duration = 1.0
         image.layer.addAnimation(animation, forKey: "position")
-        
     }
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
-    
 }
-
