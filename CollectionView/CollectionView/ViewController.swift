f8bb05ae98fc234f635a5f26421fd142e02b46cc
Carlos Butron <beiosdev@icloud.com>
2016-03-14 10:01:56 +0100

    Updated to Swift 2.1

--- CollectionView/CollectionView/ViewController.swift
+++ CollectionView/CollectionView/ViewController.swift
@@ -10,32 +10,20 @@ import UIKit
 
 class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
     
-    
     var items : NSArray = [UIImage(named:"image1.jpg")!,UIImage(named:"image2.jpg")!,UIImage(named:"image3.jpg")!,UIImage(named:"image4.jpg")!,UIImage(named:"image5.jpg")!,UIImage(named:"image6.jpg")!,UIImage(named:"image7.jpg")!,UIImage(named:"image8.jpg")!,UIImage(named:"image9.jpg")!]
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        
-
-        // Do any additional setup after loading the view, typically from a nib.
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        
-
-        // Dispose of any resources that can be recreated.
     }
     
-    
-    
     func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        
         return items.count * 5
     }
     
-
     func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
         let identifier: String = "CollectionCell"
         let cell: UICollectionViewCell = collectionView.dequeueReusableCellWithReuseIdentifier(identifier, forIndexPath: indexPath) 
@@ -44,10 +32,5 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
         imageView.image = items.objectAtIndex(indexPath.row%9) as? UIImage
         return cell
     }
-    
-    
-
-
 
 }
-
