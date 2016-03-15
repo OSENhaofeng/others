d208e5afef837c8c8c62e47c8fcaa360eca03e55
Carlos Butron <beiosdev@icloud.com>
2016-03-15 09:48:05 +0100

    Updated to Swift 2.1

--- CoreAnimationSample3/CoreAnimationSample3/ViewController.swift
+++ CoreAnimationSample3/CoreAnimationSample3/ViewController.swift
@@ -15,25 +15,16 @@ class ViewController: UIViewController {
     @IBOutlet weak var image: UIImageView!
     @IBAction func animate(sender: UIButton) {
         
-        
-        
         if (position){  //SAMPLE2
             
             let animation:CABasicAnimation! = CABasicAnimation(keyPath:"position")
-            
-            
             animation.toValue = NSValue(CGPoint:CGPointMake(160, 200))
             
             //SAMPLE2
             animation.fillMode = kCAFillModeForwards
             animation.removedOnCompletion = false
-            
-            
             let resizeAnimation:CABasicAnimation = CABasicAnimation(keyPath:"bounds.size")
             resizeAnimation.toValue = NSValue(CGSize:CGSizeMake(240, 60))
-            
-            
-            
             
             //SAMPLE2
             resizeAnimation.fillMode = kCAFillModeForwards
@@ -41,7 +32,6 @@ class ViewController: UIViewController {
             
             
             //SAMPLE3
-            
             UIView.animateWithDuration(5.0, animations:{
                 //PROPERTIES CHANGES TO ANIMATE
                 self.image.alpha = 0.0
@@ -50,54 +40,38 @@ class ViewController: UIViewController {
                     //when finished animation do this..
                     self.image.alpha = 1.0
                     self.image.layer.addAnimation(animation, forKey: "position")
-                    
                     self.image.layer.addAnimation(resizeAnimation, forKey: "bounds.size")
             })
-            
-            
-            
-            
-            
-            
             position = false
         }
         else{
             
             let animation:CABasicAnimation! = CABasicAnimation(keyPath:"position")
-            
             animation.fromValue = NSValue(CGPoint:CGPointMake(160, 200))
             
             //SAMPLE2
             animation.fillMode = kCAFillModeForwards
             animation.removedOnCompletion = false
-            
             let resizeAnimation:CABasicAnimation = CABasicAnimation(keyPath:"bounds.size")
             resizeAnimation.fromValue = NSValue(CGSize:CGSizeMake(240, 60))
             
             //SAMPLE2
             resizeAnimation.fillMode = kCAFillModeForwards
             resizeAnimation.removedOnCompletion = false
-            
             image.layer.addAnimation(animation, forKey: "position")
-            
             image.layer.addAnimation(resizeAnimation, forKey: "bounds.size")
             
             position = true
         }
-        
     }
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
-    
 }
-
