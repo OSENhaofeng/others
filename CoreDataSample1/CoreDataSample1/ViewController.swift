80432151c10fb3184994dfbfbaea3e2109440dd8
Carlos Butron <beiosdev@icloud.com>
2016-03-17 12:04:02 +0100

    Updated to Swift 2.1

--- CoreDataSample1/CoreDataSample1/ViewController.swift
+++ CoreDataSample1/CoreDataSample1/ViewController.swift
@@ -9,76 +9,62 @@
 import UIKit
 import CoreData
         
-
 class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
     var results : NSArray?
     
     override func viewDidLoad() {
-            super.viewDidLoad()
+        super.viewDidLoad()
         
         let appDel: AppDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
         let context : NSManagedObjectContext = appDel.managedObjectContext
         
+        // INSERT
+        let celda = NSEntityDescription.insertNewObjectForEntityForName("Cell", inManagedObjectContext:  context) 
+        celda.setValue("Yoda Tux", forKey: "title")
+        celda.setValue("Science Fiction", forKey: "subtitle")
+        celda.setValue("yodaTux.png", forKey: "image")
         
-       // INSERT
-        
-                let celda = NSEntityDescription.insertNewObjectForEntityForName("Cell", inManagedObjectContext:  context) 
-                celda.setValue("Yoda Tux", forKey: "title")
-                celda.setValue("Science Fiction", forKey: "subtitle")
-                celda.setValue("yodaTux.png", forKey: "image")
-        
-                do {try context.save()}
-                catch {
-                    print("Error!")
-                }
-        
+        do { 
+           try context.save()
+        }
+        catch {
+           print("Error!")
+        }
 
+        let request = NSFetchRequest (entityName: "Cell")
+        request.returnsObjectsAsFaults = false
+        results = try? context.executeFetchRequest(request)
             
-            let request = NSFetchRequest (entityName: "Cell")
-            request.returnsObjectsAsFaults = false
-            
-            results = try? context.executeFetchRequest(request)
-            
-            if (results!.count>0){
-                
-                for res in results! {
-                print(res)
-                }
-                
+        if (results!.count>0){
+            for res in results! {
+            print(res)
             }
-            
-            // Do any additional setup after loading the view, typically from a nib.
+        }
     }
     
     override func didReceiveMemoryWarning() {
-                    super.didReceiveMemoryWarning()
-                    // Dispose of any resources that can be recreated.
+        super.didReceiveMemoryWarning()
     }
-    
-    
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
                         
-                        let cell:UITableViewCell = UITableViewCell(style: UITableViewCellStyle.Subtitle, reuseIdentifier: nil)
+        let cell:UITableViewCell = UITableViewCell(style: UITableViewCellStyle.Subtitle, reuseIdentifier: nil)
+        let aux = results![indexPath.row] as! NSManagedObject
                         
-                        let aux = results![indexPath.row] as! NSManagedObject
+        cell.textLabel!.text = aux.valueForKey("title") as? String
+        cell.detailTextLabel?.text = aux.valueForKey("subtitle") as? String
+        cell.imageView!.image = UIImage(named: aux.valueForKey("image") as! String)
                         
-                        cell.textLabel!.text = aux.valueForKey("title") as? String
-                        cell.detailTextLabel?.text = aux.valueForKey("subtitle") as? String
-                        cell.imageView!.image = UIImage(named: aux.valueForKey("image") as! String)
-                        
-                        return cell
+        return cell
     }
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-                            return results!.count
+        return results!.count
     }
     
     func tableView(tableView: UITableView, titleForHeaderInSection section: Int)-> String?  {
         return "TuxMania"
     }
     
-    
 }
-
