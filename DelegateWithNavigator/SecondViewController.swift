c1d50494aa4598c6baf61549c36f9e922ba129a8
Carlos Butron <beiosdev@icloud.com>
2016-03-24 15:48:05 +0100

    Updated to Swift 2.1

--- DelegateWithNavigator/SecondViewController.swift
+++ DelegateWithNavigator/SecondViewController.swift
@@ -6,7 +6,6 @@
 //  Copyright (c) 2015 Carlos Butron. All rights reserved.
 //
 
-
 import UIKit
 
 protocol myDelegate {
@@ -37,10 +36,8 @@ class SecondViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    
     /*
     // MARK: - Navigation
-    
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepareForSegue(segue: UIStoryboardSegue!, sender: AnyObject!) {
     // Get the new view controller using segue.destinationViewController.
