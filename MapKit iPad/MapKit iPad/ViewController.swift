bd141b9157f9847fbe65295233bd6d225c4d150e
Carlos Butron <beiosdev@icloud.com>
2016-03-26 00:53:02 +0100

    Updated to Swift 2.1

--- MapKit iPad/MapKit iPad/ViewController.swift
+++ MapKit iPad/MapKit iPad/ViewController.swift
@@ -9,8 +9,6 @@
 import UIKit
 import MapKit
 
-
-
 class ViewController: UIViewController, MKMapViewDelegate {
     
     @IBOutlet weak var latitude: UILabel!
@@ -22,54 +20,31 @@ class ViewController: UIViewController, MKMapViewDelegate {
     @IBAction func createAnotation(sender: AnyObject) {
         
         let a = MyAnotation(c: myMap.centerCoordinate, t: "Center", st: "The map center")
-        
         mapView(myMap, viewForAnnotation: a)
-        
-        
         myMap.addAnnotation(a)
-        
-        
         set.addObject(a)
-        
-        
     }
     
     @IBAction func deleteAnotation(sender: AnyObject) {
         //new for swift 2.0
         let annotationsToRemove = self.myMap.annotations
         self.myMap.removeAnnotations(annotationsToRemove)
-
-
-// old swift 1.2
-//        for (var i=0; i<myMap.annotations.count; i++) {
-//            myMap.removeAnnotations(set as [AnyObject] as [AnyObject])
-//        }
-        
-        
     }
     
     @IBAction func coordinates(sender: AnyObject) {
-        
-        
+ 
         latitude.text = "\(myMap.centerCoordinate.latitude)"
         longitude.text = "\(myMap.centerCoordinate.longitude)"
-        
-        
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         self.myMap.delegate = self
-        // Do any additional setup after loading the view, typically from a nib.
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-    
-    
     
     func mapView(mapView: MKMapView, viewForAnnotation annotation:
         MKAnnotation) -> MKAnnotationView?{
@@ -81,9 +56,4 @@ class ViewController: UIViewController, MKMapViewDelegate {
             return pinView
     }
     
-    
-    
 }
-
-
-
