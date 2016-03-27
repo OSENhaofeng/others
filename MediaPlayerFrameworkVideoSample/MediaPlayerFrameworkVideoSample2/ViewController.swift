ff114a2f07de5c479dd997461cdcf813ab75f14f
Carlos Butron <beiosdev@icloud.com>
2016-03-27 20:59:49 +0200

    Updated to Swift 2.1

--- MediaPlayerFrameworkVideoSample/MediaPlayerFrameworkVideoSample2/ViewController.swift
+++ MediaPlayerFrameworkVideoSample/MediaPlayerFrameworkVideoSample2/ViewController.swift
@@ -10,40 +10,24 @@ import UIKit
 import AVKit
 import AVFoundation
 
-
 class ViewController: UIViewController {
-    
-
     
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         
             let destination = segue.destinationViewController as! AVPlayerViewController
-            
             let bundle = NSBundle.mainBundle()
             //our video is 02.mov
             let moviePath = bundle.pathForResource("02", ofType: "mov")
             let url = NSURL(fileURLWithPath: moviePath!)
             destination.player = AVPlayer(URL: url)
-    
     }
 
-    
-    
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-
     }
-    
-
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
-    
 }
-
-
