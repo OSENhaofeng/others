3e3fe2476b178e840ebf90f949d8d3c205c5a1dd
Carlos Butron <beiosdev@icloud.com>
2016-03-28 22:01:46 +0200

    Updated to Swift 2.1

--- MultiTask/MultiTask/ViewController.swift
+++ MultiTask/MultiTask/ViewController.swift
@@ -15,21 +15,15 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         NSNotificationCenter.defaultCenter().addObserver(self, selector: "updateInterface:", name: UIApplicationWillEnterForegroundNotification, object: nil)
-        // Do any additional setup after loading the view, typically from a nib.
     }
     
     func updateInterface (notification: NSNotification){
-        
         myNotification.text = "Back to background with notification"
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
-    
 }
-
