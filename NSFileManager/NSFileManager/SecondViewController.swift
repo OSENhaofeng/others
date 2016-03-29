859a9f3721c92547c0894af9a132fa08a44babc9
Carlos Butron <beiosdev@icloud.com>
2016-03-29 09:51:03 +0200

    Updated to Swift 2.1

--- NSFileManager/NSFileManager/SecondViewController.swift
+++ NSFileManager/NSFileManager/SecondViewController.swift
@@ -13,22 +13,16 @@ class SecondViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var name: UITextField!
     @IBOutlet weak var text: UITextView!
     @IBOutlet weak var nameDirectory: UITextField!
-    
-    
-    
+
     @IBAction func createFile(sender: UIButton) {
-        
         let content = text.text.dataUsingEncoding(NSUTF8StringEncoding, allowLossyConversion: true)
         if(fileManager.createFileAtPath(documentsPath.stringByAppendingPathComponent("\(name.text).txt"), contents: content, attributes: nil)){
             print("created")
         }
-        
     }
     
     @IBAction func createDirectory(sender: UIButton) {
-            
         let newDirectory:NSString = documentsPath.stringByAppendingPathComponent(nameDirectory.text!)
-        
         let fileManager = NSFileManager.defaultManager()
         
         _ = try? fileManager.createDirectoryAtPath( newDirectory as String,
@@ -38,7 +32,6 @@ class SecondViewController: UIViewController, UITextFieldDelegate {
 //        if(fileManager.createDirectoryAtPath(newDirectory as String, withIntermediateDirectories: true, attributes: nil)){
 //            print("created")
 //        }
-        
     }
     
     var fileManager = NSFileManager.defaultManager()
@@ -46,16 +39,12 @@ class SecondViewController: UIViewController, UITextFieldDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         name.delegate = self
         nameDirectory.delegate = self
-
-        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
     func textFieldShouldReturn(textField: UITextField) -> Bool // called when 'return' key pressed. return NO to ignore.
@@ -67,16 +56,5 @@ class SecondViewController: UIViewController, UITextFieldDelegate {
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
         self.view.endEditing(true)
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
