77fad529aff846494832b5fbdb25bb001c907497
Carlos Butron <beiosdev@icloud.com>
2016-03-31 11:37:17 +0200

    Updated to Swift 2.1

--- NavigationController/NavigationController/ViewController.swift
+++ NavigationController/NavigationController/ViewController.swift
@@ -5,14 +5,6 @@
 //  Created by Carlos Butron on 02/12/14.
 //  Copyright (c) 2014 Carlos Butron.
 //
-//  This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
-//  License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later
-//  version.
-//  This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
-//  warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
-//  You should have received a copy of the GNU General Public License along with this program. If not, see
-//  http:/www.gnu.org/licenses/.
-//
 
 import UIKit
 
@@ -20,14 +12,10 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
-
 }
-
