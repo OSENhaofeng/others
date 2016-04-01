a178aadf60b2298f09325986c71391a48fa8c3e7
Carlos Butron <beiosdev@icloud.com>
2016-04-01 09:37:33 +0200

    Updated to Swift 2.1

--- Protocol-Delegate/Protocol-Delegate/ViewController.swift
+++ Protocol-Delegate/Protocol-Delegate/ViewController.swift
@@ -12,23 +12,17 @@ class ViewController: UIViewController, myDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         let myRequest: myObject = myObject()
         myRequest.delegate = self
         myRequest.start()
-        
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
     func delegateMethod() {
             print("Received message")
     }
 
-
 }
-
