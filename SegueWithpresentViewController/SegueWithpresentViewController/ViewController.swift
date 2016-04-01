5011bd6fbd3ae18ebaa5e2e744da2b85bd1e3d97
Carlos Butron <beiosdev@icloud.com>
2016-04-02 00:03:31 +0200

    Updated to Swift 2.1

--- SegueWithpresentViewController/SegueWithpresentViewController/ViewController.swift
+++ SegueWithpresentViewController/SegueWithpresentViewController/ViewController.swift
@@ -13,27 +13,20 @@ class ViewController: UIViewController {
     @IBAction func buttonAction(sender: UIButton) {
         let storyboard = UIStoryboard(name: "Main", bundle: NSBundle.mainBundle())
         let viewController:UIViewController = storyboard.instantiateViewControllerWithIdentifier("NewViewController") 
-        self.presentViewController(viewController, animated: true,
-            completion: nil)
+        self.presentViewController(viewController, animated: true, completion: nil)
     }
     
-    
     @IBAction func buttonAction2(sender: UIButton) {
-        
         let newController = UIViewController (nibName: "NewClass", bundle: nil)
-        self.presentViewController(newController, animated: true,
-            completion: nil)
+        self.presentViewController(newController, animated: true, completion: nil)
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-    
     
 }
