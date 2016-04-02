6311717e8caaeadec1c07d1c894310e28cf04c8a
Carlos Butron <beiosdev@icloud.com>
2016-04-03 00:50:57 +0200

    Updated to Swift 2.1

--- SeguesCustom/Segues/ViewController.swift
+++ SeguesCustom/Segues/ViewController.swift
@@ -12,26 +12,18 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-    
-    
     
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         
         if (segue.identifier=="NewClass"){
             let myDestVC = segue.destinationViewController as! NewClassViewController
-            
             myDestVC.labelName = "text"
-            
         }
     }
     
-    
 }
-
