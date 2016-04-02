e6f4febbcb5ea22a44d215c353b8ad09a8f0c55d
Carlos Butron <beiosdev@icloud.com>
2016-04-03 00:52:29 +0200

    Updated to Swift 2.1

--- Show Local IP/showip/ViewController.swift
+++ Show Local IP/showip/ViewController.swift
@@ -13,15 +13,10 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         getIFAddresses()
-        
-
-
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
     func getIFAddresses() -> [String] {
@@ -57,6 +52,4 @@ class ViewController: UIViewController {
         return addresses
     }
 
-
 }
-
