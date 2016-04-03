80ca7439f49beb4a389a98738f2f029308b50103
Carlos Butron <beiosdev@icloud.com>
2016-04-04 00:05:19 +0200

    Updated to Swift 2.1

--- SocialFrameworkTwitter/SocialFrameworkTwitter/TimelineCell.swift
+++ SocialFrameworkTwitter/SocialFrameworkTwitter/TimelineCell.swift
@@ -20,8 +20,6 @@ class TimelineCell: UITableViewCell {
 
     override func setSelected(selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
     }
 
 }
