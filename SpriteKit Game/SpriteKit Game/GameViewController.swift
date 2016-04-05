bec2cc9fd3908cc51180199b6f218f7794ef8773
Carlos Butron <beiosdev@icloud.com>
2016-04-06 00:11:32 +0200

    Updated to Swift 2.1

--- SpriteKit Game/SpriteKit Game/GameViewController.swift
+++ SpriteKit Game/SpriteKit Game/GameViewController.swift
@@ -43,7 +43,6 @@ class GameViewController: UIViewController {
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Release any cached data, images, etc that aren't in use.
     }
     
     override func prefersStatusBarHidden() -> Bool {
