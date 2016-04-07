80b71d86872dbf01f19109e2a1369f47595668d7
Carlos Butron <beiosdev@icloud.com>
2016-04-07 21:20:30 +0200

    Updated to Swift 2.1

--- SwiftLoginScreen/SwiftLoginScreen/HomeVC.swift
+++ SwiftLoginScreen/SwiftLoginScreen/HomeVC.swift
@@ -14,8 +14,6 @@ class HomeVC: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
     
     override func viewDidAppear(animated: Bool) {
@@ -32,25 +30,12 @@ class HomeVC: UIViewController {
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-    
-
-    /*
-    // #pragma mark - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue?, sender: AnyObject?) {
-        // Get the new view controller using [segue destinationViewController].
-        // Pass the selected object to the new view controller.
-    }
-    */
 
     @IBAction func logoutTapped(sender : UIButton) {
         
         let appDomain = NSBundle.mainBundle().bundleIdentifier
         NSUserDefaults.standardUserDefaults().removePersistentDomainForName(appDomain!)
-        
         self.performSegueWithIdentifier("goto_login", sender: self)
     }
 }
