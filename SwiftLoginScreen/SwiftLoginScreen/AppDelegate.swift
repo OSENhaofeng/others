5009f7b5593c06914adda39aca6ab8676b5d00c8
Carlos Butron <beiosdev@icloud.com>
2016-04-07 21:16:42 +0200

    Updated to Swift 2.1

--- SwiftLoginScreen/SwiftLoginScreen/AppDelegate.swift
+++ SwiftLoginScreen/SwiftLoginScreen/AppDelegate.swift
@@ -13,7 +13,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
                             
     var window: UIWindow?
 
-
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         /*
         self.window = UIWindow(frame: UIScreen.mainScreen().bounds)
@@ -46,6 +45,4 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
 
-
 }
-
