d718e6ffca6bc8c55af0784cf06f49748525365a
Carlos Butron <beiosdev@icloud.com>
2016-04-08 11:49:13 +0200

    Updated to Swift 2.1

--- UILocalNotification/UILocalNotification/ViewController.swift
+++ UILocalNotification/UILocalNotification/ViewController.swift
@@ -23,9 +23,6 @@ class ViewController: UIViewController {
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
-    
 }
-
