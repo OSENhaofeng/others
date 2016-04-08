ee47ffdfd3ffbed480f59af559d914676aaebd91
Carlos Butron <beiosdev@icloud.com>
2016-04-08 11:56:54 +0200

    Updated to Swift 2.1

--- iAdFramework/iAdFramework/ViewController.swift
+++ iAdFramework/iAdFramework/ViewController.swift
@@ -21,14 +21,10 @@ class ViewController: UIViewController, ADBannerViewDelegate {
         bannerView.center = self.view.center
         bannerView.hidden = false
         self.view.addSubview(bannerView)
-        // Do any additional setup after loading the view, typically from a nib.
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
-    
 }
-
