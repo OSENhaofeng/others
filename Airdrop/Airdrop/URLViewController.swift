cf316c77b87883602425f1ad33d1991bbc31bc48
Carlos Butron <carlosbutron@icloud.com>
2016-04-10 19:02:06 +0200

    Updated to Swift 2.2

--- Airdrop/Airdrop/URLViewController.swift
+++ Airdrop/Airdrop/URLViewController.swift
@@ -12,6 +12,7 @@ class URLViewController: UIViewController, UIWebViewDelegate {
     
     @IBOutlet weak var myURL: UITextField!
     @IBOutlet weak var webView: UIWebView!
+    @IBOutlet weak var webView2: UIWebView!
     
     @IBAction func send(sender: UIButton) {
         let url:NSURL = NSURL(string: myURL.text!)!
@@ -21,15 +22,32 @@ class URLViewController: UIViewController, UIWebViewDelegate {
     
     @IBAction func load(sender: UIButton) {
         let request = NSURLRequest(URL: NSURL(string: myURL.text!)!)
-        self.webView.loadRequest(request)
+        self.webView2.loadRequest(request)
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        myURL.text = "http://carlosbutron.es/"
-        webView.delegate = self
-        let request = NSURLRequest(URL: NSURL(string: myURL.text!)!)
-        self.webView.loadRequest(request)
+        
+        webView2.delegate = self
+        
+        //1. Load web site into my web view
+        
+        let myURL = NSURL(string: "http://www.swiftdeveloperblog.com");
+        let myURLRequest:NSURLRequest = NSURLRequest(URL: myURL!);
+        webView2.loadRequest(myURLRequest);
+        
+        
+        
+        
+//        // Do any additional setup after loading the view, typically from a nib.
+//        let url = NSURL (string: "http://www.google.com");
+//        let request = NSURLRequest(URL: url!);
+//        webView2.loadRequest(request);
+        
+//        myURL.text = "http://carlosbutron.es/"
+//        webView2.delegate = self
+//        let request = NSURLRequest(URL: NSURL(string: myURL.text!)!)
+//        self.webView2.loadRequest(request)
     }
 
     override func didReceiveMemoryWarning() {
