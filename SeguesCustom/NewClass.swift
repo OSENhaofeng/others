1fc20feb97342a98587db23c1fe308559e475590
Carlos Butron <carlosbtn@icloud.com>
2016-10-06 14:25:55 +0200

    Updated to Swift 3

--- SeguesCustom/NewClass.swift
+++ SeguesCustom/NewClass.swift
@@ -12,27 +12,27 @@ class NewClass: UIStoryboardSegue {
     
     override func perform(){
         
-        let source : UIViewController = self.sourceViewController 
-        let destination : UIViewController = self.destinationViewController 
+        let source : UIViewController = self.source 
+        let destination : UIViewController = self.destination 
         
         UIGraphicsBeginImageContext(source.view.bounds.size)
-        source.view.layer.renderInContext(UIGraphicsGetCurrentContext()!)
+        source.view.layer.render(in: UIGraphicsGetCurrentContext()!)
         
-        let sourceImage : UIImage = UIGraphicsGetImageFromCurrentImageContext()
-        destination.view.layer.renderInContext(UIGraphicsGetCurrentContext()!)
-        let destinationImage : UIImage = UIGraphicsGetImageFromCurrentImageContext()
+        let sourceImage : UIImage = UIGraphicsGetImageFromCurrentImageContext()!
+        destination.view.layer.render(in: UIGraphicsGetCurrentContext()!)
+        let destinationImage : UIImage = UIGraphicsGetImageFromCurrentImageContext()!
         UIGraphicsEndImageContext()
         let sourceImageView : UIImageView = UIImageView (image: sourceImage)
         let destinationImageView : UIImageView = UIImageView (image: destinationImage)
         source.view.addSubview(sourceImageView)
         source.view.addSubview(destinationImageView)
-        destinationImageView.transform = CGAffineTransformMakeTranslation(destinationImageView.frame.size.width, 0)
+        destinationImageView.transform = CGAffineTransform(translationX: destinationImageView.frame.size.width, y: 0)
         
-        UIView.animateWithDuration(1.0, animations: { () in
-            sourceImageView.transform = CGAffineTransformMakeTranslation(-sourceImageView.frame.size.width, 0)
-            destinationImageView.transform = CGAffineTransformMakeTranslation(0, 0)
+        UIView.animate(withDuration: 1.0, animations: { () in
+            sourceImageView.transform = CGAffineTransform(translationX: -sourceImageView.frame.size.width, y: 0)
+            destinationImageView.transform = CGAffineTransform(translationX: 0, y: 0)
             }, completion: { (finished) in
-                source.presentViewController(destination, animated: true, completion: nil)
+                source.present(destination, animated: true, completion: nil)
         }) }
     
 }
