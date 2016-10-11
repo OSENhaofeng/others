e07bc41d7b5b5a45d61cec54be79c9ed2e7b454e
Carlos Butron <carlosbtn@icloud.com>
2016-10-12 01:33:00 +0200

    Updated to Swift 3

--- NSURLSessionDownloadDelegate/NSURLSessionDownloadDelegate/ViewController.swift
+++ NSURLSessionDownloadDelegate/NSURLSessionDownloadDelegate/ViewController.swift
@@ -8,41 +8,41 @@
 
 import UIKit
 
-class ViewController: UIViewController, NSURLSessionDownloadDelegate {
+class ViewController: UIViewController, URLSessionDownloadDelegate {
     
-    var session = NSURLSession()
+    var session = Foundation.URLSession()
     
     @IBOutlet weak var imagen: UIImageView!
     @IBOutlet weak var progreso: UIProgressView!
     
-    @IBAction func cargar(sender: UIButton) {
+    @IBAction func cargar(_ sender: UIButton) {
         let imageUrl: NSString = "http://c.hiphotos.baidu.com/image/pic/item/8cb1cb13495409235fa14adf9158d109b2de4942.jpg"
-        let getImageTask: NSURLSessionDownloadTask =
-        session.downloadTaskWithURL(NSURL(string: imageUrl as String)!)
+        let getImageTask: URLSessionDownloadTask =
+        session.downloadTask(with: URL(string: imageUrl as String)!)
         getImageTask.resume()
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         let sessionConfig =
-        NSURLSessionConfiguration.defaultSessionConfiguration()
-        session = NSURLSession(configuration: sessionConfig, delegate: self, delegateQueue: nil)
+        URLSessionConfiguration.default
+        session = Foundation.URLSession(configuration: sessionConfig, delegate: self, delegateQueue: nil)
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
     
-    func URLSession(session: NSURLSession, downloadTask: NSURLSessionDownloadTask, didFinishDownloadingToURL location: NSURL){
+    func urlSession(_ session: URLSession, downloadTask: URLSessionDownloadTask, didFinishDownloadingTo location: URL){
         print("Download finished")
-        let downloadedImage = UIImage(data: NSData(contentsOfURL: location)!)
-        dispatch_async(dispatch_get_main_queue(), {() in
+        let downloadedImage = UIImage(data: try! Data(contentsOf: location))
+        DispatchQueue.main.async(execute: {() in
             self.imagen.image = downloadedImage
         })
     }
     
-    func URLSession(session: NSURLSession, downloadTask: NSURLSessionDownloadTask, didWriteData bytesWritten: Int64,totalBytesWritten: Int64, totalBytesExpectedToWrite: Int64){
-        dispatch_async(dispatch_get_main_queue(), {() in
+    func urlSession(_ session: URLSession, downloadTask: URLSessionDownloadTask, didWriteData bytesWritten: Int64,totalBytesWritten: Int64, totalBytesExpectedToWrite: Int64){
+        DispatchQueue.main.async(execute: {() in
             let variable = Float(totalBytesWritten)/Float(totalBytesExpectedToWrite)
             self.progreso.progress = variable
         }) }
